f29a3944cedecb3eb46612e3fb8365b9a7e50842
Andreas <202204219@post.au.dk>
2024-11-20 13:20:45 +0100

    Brugernavn: user Password: password

--- app/src/main/java/com/example/techtrackr/MainActivity.kt
+++ app/src/main/java/com/example/techtrackr/MainActivity.kt
@@ -3,45 +3,32 @@ package com.example.techtrackr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.techtrackr.ui.theme.TechTrackrTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TechTrackrTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // Set up the navigation controller
+                val navController = rememberNavController()
+                // NavHost to manage the screens
+                NavHost(navController = navController, startDestination = "login") {
+                    composable("login") {
+                        // Pass the navController to the LoginScreen
+                        LoginScreen(navController = navController)
+                    }
+                    composable("main") {
+                        MainScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TechTrackrTheme {
-        Greeting("Android")
     }
 }
