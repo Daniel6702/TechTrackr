4e51c4daa2560a58b4f590a3fd59620840aa9f45
Daniel Mærsk Pedersen <pedersendaniel3561@gmail.com>
2024-12-14 22:14:23 +0100

    notification permission fix

--- app/src/main/java/com/example/techtrackr/workers/PriceMonitorWorker.kt
+++ app/src/main/java/com/example/techtrackr/workers/PriceMonitorWorker.kt
@@ -2,11 +2,14 @@ package com.example.techtrackr.workers
 
 // package com.example.techtrackr.workers
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
@@ -114,6 +117,20 @@ class PriceMonitorWorker(
 
         // Show Notification
         with(NotificationManagerCompat.from(applicationContext)) {
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
             notify(notificationId, builder.build())
         }
     }
